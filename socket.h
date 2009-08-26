c9588c59ff055338ec5529450a6eff9ef5b56a6d
akabigbro <akabigbro@1bbc7a9c-619d-11de-84d6-ef71daef37f8>
2009-08-26 20:11:34 +0000

    merging ssapi.20090625

--- socket.h
+++ socket.h
@@ -7,20 +7,22 @@
 
 class Socket
 {
-protected:
-    int fd;
+    protected:
+        int fd;
 
-    virtual ssize_t Read(int fd, char * buffer, size_t max, int flags) throw(int&)=0;
-    virtual ssize_t Send(int fd, const char * buffer, size_t max, int flags) throw(int&)=0;
-public:
-    Socket(void) {};
-    Socket(int domain, int type, int protocol);
-    ~Socket(void) throw(int&);
+        virtual ssize_t Read(int fd, char * buffer, size_t max, int flags) throw(int&)=0;
+        virtual ssize_t Send(int fd, const char * buffer, size_t max, int flags) throw(int&)=0;
+    public:
+        Socket(void) {};
+        Socket(int domain, int type, int protocol);
+        ~Socket(void) throw(int&);
 
-    void ShutDown(int how) throw(int&);
+        void ShutDown(int how) throw(int&);
 
-    virtual ssize_t Read(char * buffer, size_t max, int flags = 0) throw(int&);
-    virtual ssize_t Send(const char * buffer, size_t max, int flags = 0) throw(int&);
+        virtual ssize_t Read(char * buffer, size_t max, int flags = 0) throw(int&);
+        virtual ssize_t Send(const char * buffer, size_t max, int flags = 0) throw(int&);
+
+        void SetOption(int level, int option, const void * value, socklen_t length) throw(int&);
 };
 
 #endif//SOCKET_H
